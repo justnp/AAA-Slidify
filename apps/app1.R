2d3b7c072f6fea945870745a365eed0b631e69fa
justnp <just_np@yahoo.com>
2014-10-13 22:58:40 +1100

    uploaded

--- apps/app1.R
+++ apps/app1.R
@@ -1,8 +1,8 @@
 library(rCharts)
 library(slidify)
+
 # Read airtraffic data
-setwd("C:/AAA-Slidify")
-auAirTrafficRaw <- read.csv("auAirTraffic_V2.csv")
+auAirTrafficRaw <- read.csv("/AAA-Slidify/auAirTraffic_V2.csv")
 
 # Remove the records without blank Ranks
 auAirTraffic <- subset(auAirTrafficRaw, RANK != '-')
